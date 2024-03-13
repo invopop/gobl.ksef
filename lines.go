a5321e6a38562f79259afb86e932d2ff106d9268
Grzegorz Lisowski <glisowski@fractalsoft.org>
2024-03-13 15:41:26 +0100

    Make UnitDiscount field not visible when the value is zero
    The original way we set the UnitDiscount field it was returning a 0.00
    string which was causing the field to be visible in the invoice in
    every case. This is not the way we want it to work. The field should
    only be visible if the discount amount is different than zero.

--- lines.go
+++ lines.go
@@ -27,12 +27,22 @@ func newLine(line *bill.Line) *Line {
 		Measure:       string(line.Item.Unit.UNECE()),
 		NetUnitPrice:  line.Item.Price.String(),
 		Quantity:      line.Quantity.String(),
-		UnitDiscount:  line.Total.Subtract(line.Sum).Divide(line.Quantity).String(), // not sure if there should be some rescale or matchPrecision
+		UnitDiscount:  unitDiscount(line),
 		NetPriceTotal: line.Total.String(),
 		TaxRate:       line.Taxes[0].Percent.Rescale(2).StringWithoutSymbol(),
 	}
 
 	return Line
+}
+
+func unitDiscount(line *bill.Line) string {
+	if line.Total == line.Sum {
+		return ""
+	}
+
+	discount := line.Total.Subtract(line.Sum).Divide(line.Quantity) // not sure if there should be some rescale or matchPrecision
+
+	return discount.String()
 }
 
 // NewLines generates lines for the KSeF invoice
